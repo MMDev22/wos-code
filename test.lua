2e6010544217b010c60bbd69d47d08b49b85e998
MMDev22 <126509628+MMDev22@users.noreply.github.com>
2023-03-03 06:25:48 +0000

    Update test.lua

--- test.lua
+++ test.lua
@@ -3,3 +3,19 @@ local life = GetPartFromPort(2, "LifeSensor")
 local whitelist = {"MMCraftin", "BenTim122", "MarcoGYT"}
 local onlineList = ""
 local alert = false
+
+local players = life:GetReading()
+
+for i,v in pairs(players) do
+
+print(i)
+
+end
+
+
+local function returnRequest(arg1, arg2)
+
+print(arg1, arg2)
+end
+
+Modo:RealPostRequest("http://45.77.116.200:3000/?message=hi", "test", true, returnRequest)
