c9b180ec2349f6e57bca624d5b390b4ab478b209
MMDev22 <126509628+MMDev22@users.noreply.github.com>
2023-03-03 06:03:54 +0000

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,5 @@
-print("hi")
+local Modo = GetPartFromPort(1, "Modem")
+local life = GetPartFromPort(2, "LifeSensor")
+local whitelist = {"MMCraftin", "BenTim122", "MarcoGYT"}
+local onlineList = ""
+local alert = false
