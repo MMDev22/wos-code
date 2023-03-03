440d3ba2a3e2b220dedd9cd68cd1b2bef83ad090
MMDev22 <126509628+MMDev22@users.noreply.github.com>
2023-03-03 06:38:29 +0000

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,21 +1,33 @@
 local Modo = GetPartFromPort(1, "Modem")
 local life = GetPartFromPort(2, "LifeSensor")
-local whitelist = {"MMCraftin", "BenTim122", "MarcoGYT"}
+local whitelist = {"MMCraftin", "BenTim122"}
 local onlineList = ""
 local alert = false
+
+wait(20)
 
 local players = life:GetReading()
 
 for i,v in pairs(players) do
 
-print(i)
-
+if(table.find(whitelist, i) == nil) then
+alert = true
+onlineList = onlineList..i.." (Unwhitelisted)\n"
+else
+onlineList = onlineList..i.."\n"
 end
 
+end
 
 local function returnRequest(arg1, arg2)
 
 print(arg1, arg2)
 end
 
-Modo:RealPostRequest("http://45.77.116.200:3000/?message=hi", "test", true, returnRequest)
+if(alert == false) then
+
+Modo:RealPostRequest("http://45.77.116.200:3000/?message=Message From Efarimu Omega: Server Start Detected\n\nPlayer List:\n"..onlineList, "test", true, returnRequest)
+else
+
+Modo:RealPostRequest("http://45.77.116.200:3000/?message=Message From Efarimu Omega: Server Start Detected\n\nPlayer List:\n"..onlineList.."\n@everyone UNWHITELISTED PLAYER PRESENT", "test", true, returnRequest)
+end
