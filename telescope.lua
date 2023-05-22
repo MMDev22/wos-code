9ef108317af1560ea7b826d23ae21372461bf2ee
MMDev22 <126509628+MMDev22@users.noreply.github.com>
2023-05-22 04:22:10 +0000

    Update telescope.lua

--- telescope.lua
+++ telescope.lua
@@ -1,8 +1,7 @@
 local x = -101
 local y = -101
-local z = 0
-local t = 0
-local countT = 0
+local z = -101
+local t = -101
 
 local success, err = pcall(function()
   local Tele = GetPartFromPort(1, "Telescope")
@@ -11,13 +10,17 @@ local success, err = pcall(function()
     y = -101
       for count=1, 201 do
         y = y + 1
-        countT = countT + 1
-        --Tele:Configure({["ViewCoordinates"] = x..","..y..","..z..","..t..",false"})
-        --print(JSONEncode(Tele:GetCoordinate()))
-        --print(x..","..y..","..z..","..t..",false")
-        --print(countT)
+        z = -101
+          for count=1, 201 do
+             z = z + 1
+             t = -101
+             for count=1, 201 do
+                t = t + 1
+             end
+          end
       end
       print(x.." done")
+      wait(30)
   end
 end)
 
